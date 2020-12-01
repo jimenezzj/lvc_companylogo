08b4700d4553ce71e75268472edc3c5dd158ef5f
Jonathan Jimenez Zambrana <jimenezzjti99@gmail.com>
2020-11-30 20:26:18 -0600

    folder restructured

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express();
 
 const PORT = 8085;
 
-app.use(express.static(path.join(require.main.filename, '..', '..', 'public')));
+app.set('port', process.env.PORT || PORT);
+
+app.use(express.static(path.join(require.main.filename, '..', 'public')));
 console.log(path.join(require.main.filename, '..', '..', 'public'), 'HERE');
 
 app.get('/', (req, res, next) => {
     res.render('index');
 });
 
-app.listen(PORT, () => {
-    console.log('Server is running on port: ' + PORT);
+app.listen(app.get('port'), () => {
+    console.log('Server is running on port: ' + app.get('port'));
 });
